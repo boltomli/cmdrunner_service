f01ca0f6ea755895f6c04925e6cecb9a89fd6ed0
Song Li <songl@outlook.com>
2022-08-01 11:58:27 +0800

    separate grpc service

---
+++ {{cookiecutter.directory_name}}/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+grpc=5555
+http=8888
+worker=2
+
+while getopts g:h:w: flag
+do
+  case "${flag}" in
+    g) grpc=${OPTARG} ;;
+    h) http=${OPTARG} ;;
+    w) worker=${OPTARG} ;;
+    *) echo "usage: $0 [-g grpc_port] [-h http_port] [-w worker_num]" >&2
+     exit 1 ;;
+   esac
+done
+
+homi run {{cookiecutter.file_name}}_grpc.py -p ${grpc} -w ${worker} &
+
+wait -n
+exit $?
